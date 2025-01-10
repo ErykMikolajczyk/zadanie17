6fcc61ab90a81b6c6ea265fcd62dfe4ee05c94b7
FireDoggo123 <bruv1@interia.pl>
2025-01-10 10:38:17 +0100

    kod

---
+++ zadanie17/plik.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float volume_cuboid(float x, float y, float h) {
+    return (x*y*h);
+}
+
+int main() {
+    float x, y, h;
+
+    printf("Podaj dlugosc podstawy prostopadloscianu: ");
+    scanf("%f", &x);
+
+    printf("Podaj szerokosc podstawy prostopadloscianu: ");
+    scanf("%f", &y);
+
+    printf("Podaj wysokosc podstawy prostopadloscianu: ");
+    scanf("%f", &h);
+
+    printf ("Objetosc prostopadloscianu : %f\n", volume_cuboid(x, y, h));
+
+    return(0);
+}
+
+
+
+
+
+
